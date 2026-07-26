c95fe69c390621e06c300258c574395dfdb87510
jreleaserbot <jreleaser@kordamp.org>
2026-07-26 18:39:48 +0200

    viset v0.1.0

--- Formula/viset.rb
+++ Formula/viset.rb
@@ -5,7 +5,7 @@ class Viset < Formula
   homepage "https://github.com/getviset/Viset"
   url "https://github.com/getviset/Viset/releases/download/v0.1.0/viset-v0.1.0-osx-arm64.zip"
   version "0.1.0"
-  sha256 "85d2fb60a9b7f69bbb19b42e86580b8a01afa596700625837cd8ddf3481cfbb0"
+  sha256 "0357014c11c2e115c0aebaf5045db2cee149b7f39fb38254cffa3badc1d2f765"
   license "MIT"
 
 
