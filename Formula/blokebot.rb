9bd7d6941054119f6edae47880b826b850cee02e
jreleaserbot <jreleaser@kordamp.org>
2026-08-09 14:45:43 +0200

    blokebot v0.7.2

--- Formula/blokebot.rb
+++ Formula/blokebot.rb
@@ -3,9 +3,9 @@
 class Blokebot < Formula
   desc "Free, open-source Twitch bot and dashboard"
   homepage "https://www.blokebot.com/"
-  url "https://github.com/alsi-lawr/BlokeBot/releases/download/v0.7.1/blokebot-v0.7.1-osx-arm64.zip"
-  version "0.7.1"
-  sha256 "4a8133d0524429ac06a280e07e09e6595ba311cf9103d454aa2739c8a882a7ce"
+  url "https://github.com/alsi-lawr/BlokeBot/releases/download/v0.7.2/blokebot-v0.7.2-osx-arm64.zip"
+  version "0.7.2"
+  sha256 "87e4011a10945285a71b16de85385e54bde9cb2344ea7aae88b0538490f66670"
   license "MIT"
 
 
@@ -16,6 +16,6 @@ class Blokebot < Formula
 
   test do
     output = shell_output("#{bin}/blokebot --version")
-    assert_match "0.7.1", output
+    assert_match "0.7.2", output
   end
 end
